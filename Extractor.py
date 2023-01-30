f58d3e9cb95870d3da1c856d99713c2d7879c06e
Octávio X. Fúrio <103672525+OctavioFurio@users.noreply.github.com>
2023-01-30 18:49:37 -0300

    v2.2

--- Extractor.py
+++ Extractor.py
@@ -1,38 +1,97 @@
 
-# V2.1 - Extração radiomica de imagens .tif
+# V2.2 - Extração radiomica de imagens .tif
 # 30.01.23
 
-from __future__ import print_function
-from radiomics import featureextractor, getFeatureClasses
-
-import logging
 import os
 import sys
 import six
 import gc
-import radiomics
-import nrrd
-
 import numpy as np
-import pandas as pd
 import SimpleITK as sitk
+import radiomics
+from radiomics import featureextractor
 
-# Leitura do arquivo NRRD com a característica e coloca numa matriz
-def readNrrdData(filename): 
-        data, header = nrrd.read(filename)
-        voxels = []
-        for x in range(data.shape[0]):
-            for y in range(data.shape[1]):
-                for z in range(data.shape[2]):
-                    if not np.isnan(data[x, y, z]):
-                        voxels.append(data[x, y, z])
-        return voxels
+
+
+def main():
+    Diretorio, Parametros, Log = sys.argv[1], sys.argv[2], sys.argv[3]
+    print("\n\tDiretorio selecionado: %s\n\tParametros: %s\n\tResultados armazenados em: %s" % (Diretorio, Parametros, Log))
+    
+    count = 0
+    for path in os.scandir(Diretorio):
+        if path.is_file():
+            count += 1
+
+    print('\n\tArquivos encontrados no diretorio: ', count)
+    print("\n\tExtraindo caracteristicas...\n")
+
+    radiomics.setVerbosity(30)
+
+    FileSave(Diretorio, Parametros, Log, count)
+
+    print("\n\tConteudo do arquivo .arff resultante:\n")
+
+    print(open(Log, "r").read())
+
+
+
+#   Escrita no arquivo .arff a ser gerado.
+#   Formatação .arff padrão:
+
+    #   @Relation Nome_da_relação
+    #   
+    #   @Attribute A Numeric
+    #   @Attribute B Numeric
+    #   ...
+    #
+    #   @Data
+    #   1.212,5.634,... #imagem 1
+    #   2.323,3.237,... #imagem 2
+    #   ...
+
+def FileSave(pathToDir, params, logName, totalFiles):
+    arquivo = open(logName, "w")
+    arquivo.write("% Caracteristicas extraidas do diretorio /" + pathToDir + "\n\n@RELATION Caracteristicas_Radiomicas\n\n")
+
+    index = 1
+    for _, _, files in os.walk(pathToDir):
+        for file in sorted(files):
+            pathToFile = pathToDir + "/" + file
+
+            percentage = (int)(100 * (index-1) /(totalFiles))             
+
+            result = runExtractor(pathToFile, params)
+
+            if index != 1:
+                arquivo.write("\n\n% " + file + "\n")
+                dataWriter(arquivo, result)
+                index += 1
+
+            else: 
+                i = 0
+                for key, _ in six.iteritems(result):
+                    if i < 37:  # 37 primeiros atributos são diagnósticos, e iguais em todas as imagens
+                        i += 1
+                        continue
+                    arquivo.write("@ATTRIBUTE " + key + "\tNUMERIC\n")
+                arquivo.write("@ATTRIBUTE class\t{sim,nao}\n")  # A classe ("sim" ou "não") é adicionada manualmente em cada imagem após a execução 
+                arquivo.write("\n@DATA")
+                arquivo.write("\n\n% " + file + "\n")
+                dataWriter(arquivo, result)
+                index += 1
+                
+                del result
+                gc.collect
+
+            print("\t%3d %% concluido -> Extraindo caracteristicas de" % (percentage), file)
+
+    print("\t100 % concluido -> Imagens analisadas com sucesso.")
 
 
 
 def addDimension(image):
     imagem = sitk.ReadImage(image)
-    layers = 10
+    layers = 3
 
     eixo_x, eixo_z = imagem.GetWidth(), imagem.GetHeight()
     volume = sitk.Image([eixo_x, layers, eixo_z], imagem.GetPixelID())
@@ -40,57 +99,32 @@ def addDimension(image):
     for i in range(layers):
         imagem = sitk.PermuteAxes(sitk.JoinSeries([sitk.ReadImage(image)]),[0,2,1])
         volume = sitk.Paste(volume, imagem, [eixo_x, 1, eixo_z], [0,0,0], [0,i,0])
-
-    sitk.WriteImage(volume, str(image).lstrip("/")[0] + '.nrrd')
     
     return volume
 
 
 
-def main():    
-    Diretorio, Parametros, Log = sys.argv[1], sys.argv[2], sys.argv[3]
+# Execução do extrator
+def runExtractor(pathToImage, paramsFileName):
+    volume = addDimension(pathToImage)
 
-    settings = {'label': 160}
-
-    # Extrator
-    extractor = radiomics.featureextractor.RadiomicsFeatureExtractor(Parametros, additionalInfo=True, **settings)
-    featureClasses = getFeatureClasses()
-
-    # Extração de características Radiômicas por voxel
-    for diretorio, _, arquivos in os.walk(Diretorio):
-        for arquivo in arquivos:
-            f_name = arquivo.split('.')
-            try:
-                mask = image = addDimension(diretorio+'/'+arquivo)
-                featureVector = extractor.execute(image, mask, voxelBased=True)
-                for featureName, featureValue in six.iteritems(featureVector):
-                    if isinstance(featureValue, sitk.Image):
-                        sitk.WriteImage(100 * featureValue, "C:/ResultsFromPyrad" +'/%s-%s.nrrd'%(f_name[0], featureName)) #coloca o resultado da extração da feature num arquivo nrrd
-            except OSError as error:
-                pass
-
-    # Diretorio com os arquivos NRRD contendo o resultado
-    Diretorio = r"C:/ResultsFromPyrad"
-
-    for diretorio, subDiretorios, arquivos in os.walk(Diretorio): 
-        df = pd.DataFrame()
-        patient_code = ''
-        for arquivo in arquivos:
-            split = arquivo.split('-')
-            # Id
-            patient_code = split[0] 
-            
-            # Nome da feature
-            feature_name = split[1].split('.nrrd') 
-            features = readNrrdData(diretorio+'/'+arquivo)
-            
-            # Coloca a matriz num dataframe
-            df[feature_name[0]] = pd.Series(features) 
-            
-            # Exporta as features para csv
-            df.to_csv("C:/ResultsFromPyrad/" + patient_code + '_features.csv', index = False)
+    params = os.path.join('./', paramsFileName)
+    extractor = radiomics.featureextractor.RadiomicsFeatureExtractor(params, additionalInfo=True)
+    
+    return extractor.execute(volume, volume)
+    
 
 
-        
+# Registro dos dados de cada nova imagem no arquivo .arff resultante.
+def dataWriter(arquivo, result):
+    i = 0
+    for key, val in six.iteritems(result):
+        if i < 37:  # 37 primeiros atributos são diagnósticos, e iguais em todas as imagens
+            i += 1
+            continue
+        arquivo.write(str(val) + ',')
+
+
+
 if __name__ == "__main__":
     main()
